b5bf331a376bffafe8b96b3c9bcc961ca74fd3f4
kjy3408 <wodud2312@naver.com>
2023-05-10 17:42:09 +0900

    OAuth2 study: google login

---
+++ oauth2-back/src/main/java/com/study/oauth2/entity/Authority.java
@@ -0,0 +1,19 @@
+package com.study.oauth2.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Authority {
+	private int authorityId;
+	private int userId;
+	private int roleId;
+	
+	private Role role;
+	
+}
